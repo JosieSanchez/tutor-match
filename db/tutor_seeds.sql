f533baf1856a9b0135dffc01550a451c5cf70c3e
markkbrown1 <markkbrown1@gmail.com>
2017-05-09 17:05:55 -0400

    modified tutor,students and seeds

--- db/tutor_seeds.sql
+++ db/tutor_seeds.sql
@@ -1,6 +1,18 @@
-INSERT INTO tutors (tutor_name, subject, dates_avail, time_avail, email) VALUES ('Ahmed', 'Javascript', 'Monday', 09, 'safwat17@yahoo.com' ),
- ('Mark', 'HTML', 'Wednesday', 12, 'markkbrown1@gmail.com' ),
- ('Josie', 'Nodejs', 'Wednesday', 12, 'josie.sanchez5829@gmail.com' );
+INSERT INTO tutors (tutor_name, subject, dates_avail, time_avail, email) VALUES ('Ahmed', 'Javascript', '2017-05-08', 09, 'safwat17@yahoo.com' ),
+ ('Mark', 'HTML', '2017-05-10', 12:00:00, 'markkbrown1@gmail.com' ),
+ ('Paul', 'Sequelize', '2017-05-28', 6:00:00, 'kingsleypaulgrant15@gmail.com'),
+ ('Blair', 'Express', '2017-05-24', 4:30:00, 'thegenius@gmail.com'),
+ ('Brian', 'JQUERY', '2017-05-14', 5:15:00, 'dunkking@gmail.com'),
+ ('Jeff', 'CSS', '2017-05-21', 5:00:00, 'chiefexec@gmail.com'),
+  ('Kale', 'Javascript', '2017-05-11', 12:00:00, 'magicbrownies@gmail.com' ),
+ ('Frank', 'Sequelize', '2017-05-28', 6:00:00, 'wrx_wins@gmail.com'),
+ ('Sam', 'Express', '2017-05-24', 4:00:00, 'quietforce@gmail.com'),
+ ('Ash', 'JQUERY', '2017-05-14', 5:00:00, 'unsunghero@gmail.com'),
+ ('Melissa', 'CSS', '2017-05-21', 5:00:00, 'morganwhiz@gmail.com'),
+ ('Daniel', 'Express', '2017-05-24', 4:00:00, '305_empire@gmail.com'),
+ ('Christian', 'JQUERY', '2017-05-14', 5:45:00, 'toostrong@gmail.com'),
+ ('Laura', 'Javascript', '2017-05-21', 5:30:00, 'bosslaugh@gmail.com'),
+ ('Josie', 'Nodejs', '2017-05-12', 8:00:00, 'josie.sanchez5829@gmail.com');
 -- INSERT INTO tutors (tutor_name, subject, dates_avail, time_avail, email) VALUES ('Josie' josie.sanchez5829@gmail.com);
 -- INSERT INTO tutors (tutor_name, subject, dates_avail, time_avail, email) VALUES ('Mark');
 -- INSERT INTO tutors (tutor_name, subject, dates_avail, time_avail, email) VALUES ('Paul');
